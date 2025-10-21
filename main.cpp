a8cefd71b41b1c26a7a9a144647fed1b0f3d2497
st144581 <st144581@student.spbu.ru>
2025-10-21 12:51:53 +0300

    feat: command line

--- main.cpp
+++ main.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    unsigned int number;
-    cout << "Input number:";
-    cin >> number;
-
+int main(int argc, char* argv[]){
     unsigned int summ = 0;
     unsigned int prod = 1U;
 
-    do{
-        summ += number % 10;
-        prod *= number % 10;
-        number /= 10;
-    }while(number != 0);
-    cout << summ << ", " << prod << endl;
+    if (argc > 1){
+        char *number = argv[1];
+        int index = 0;
+        while(number[index] != '\0' && index < 11){
+            if(number[index] >= '0' && number[index] <= '9'){
+                summ += (number[index] - '0');
+                prod *= (number[index] - '0');
+            } else{
+                cout << "wrong number '" << argv[1] << "' " << endl;
+                return 1;
+            }
+            ++index;
+        }
+        cout << summ << ", " << prod << endl;
+    } else{
 
-    return 0;
+        unsigned int number;
+        cout << "Input number:";
+        cin >> number;
+
+        do{
+            summ += number % 10;
+            prod *= number % 10;
+            number /= 10;
+        }while(number != 0);
+        cout << summ << ", " << prod << endl;
+
+        return 0;
+    }
 }
